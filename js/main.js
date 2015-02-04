9984bd486c7fb9e87e43cb5838ba4385be7b9aab
jmounts <mountzer@gmail.com>
2015-02-04 17:04:31 -0600

    FadeIn

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ $(document).ready(function(){
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 50;  
+            bottom_of_window = bottom_of_window + 10;  
           
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},200);
+                $(this).animate({'opacity':'1'},100);
                     
             }
         }); 
