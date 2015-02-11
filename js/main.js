c93d6cce7088b3aaa79ce909431f646a9dafce4d
jmounts <mountzer@gmail.com>
2015-02-11 13:49:19 -0600

    blur

--- js/main.js
+++ js/main.js
@@ -48,4 +48,116 @@ $(document).ready(function(){
 
     $(window).stellar();
 
+// Paralax
+(function($){
+    $.fn.parallax = function(options){
+        var $$ = $(this);
+        offset = $$.offset();
+        var defaults = {
+            "start": 0,
+            "stop": offset.top + $$.height(),
+            "coeff": 0.95
+        };
+        var opts = $.extend(defaults, options);
+        return this.each(function(){
+            $(window).bind('scroll', function() {
+                windowTop = $(window).scrollTop();
+                if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
+                    newCoord = windowTop * opts.coeff;
+                    $$.css({
+                        "background-position": "0 "+ newCoord + "px"
+                    });
+                }
+            });
+        });
+    };
+})(jQuery);
+
+// call the plugin
+$('.billboard').parallax({ "coeff":-.08 });
+$('.overlay').parallax({ "coeff":-.08 });
+
+var $content = $('header .had')
+  , $blur    = $('header .overlay')
+  , wHeight  = $(window).height();
+
+$(window).on('resize', function(){
+  wHeight = $(window).height();
+});
+
+/*blur*/
+window.requestAnimFrame = (function()
+{
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+/**Scroller Blur**/
+function Scroller()
+{
+  this.latestKnownScrollY = 0;
+  this.ticking            = false;
+}
+
+Scroller.prototype = {
+  /**
+   * Initialize
+   */
+  init: function() {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+  },
+
+  /**
+   * Capture Scroll
+   */
+  onScroll: function() {
+    this.latestKnownScrollY = window.scrollY;
+    this.requestTick();
+  },
+
+  /**
+   * Request a Tick
+   */
+  requestTick: function() {
+    if( !this.ticking ) {
+      window.requestAnimFrame(this.update.bind(this));
+    }
+    this.ticking = true;
+  },
+
+  /**
+   * Update.
+   */
+  update: function() {
+    var currentScrollY = this.latestKnownScrollY;
+    this.ticking       = false;
+    
+    /**
+     * Do The Dirty Work Here
+     */
+    var slowScroll = currentScrollY / 4
+      , blurScroll = currentScrollY * 2;
+    
+    $content.css({
+      'transform'         : 'translateY(-' + slowScroll + 'px)',
+      '-moz-transform'    : 'translateY(-' + slowScroll + 'px)',
+      '-webkit-transform' : 'translateY(-' + slowScroll + 'px)'
+    });
+    
+    $blur.css({
+      'opacity' : blurScroll / wHeight
+    });
+  }
+};
+
+/**
+ * Attach!
+ */
+var scroller = new Scroller();  
+scroller.init();
+
 })
